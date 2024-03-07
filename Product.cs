38393019dee7cf76f7b3c1da0b91bf57e6eece2f
ragha <ragha@RAGHAD>
2024-03-07 03:01:42 +0200

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int QuantityInStock { get; private set; }
+
+        public Product(string name, double price, int quantityInStock)
+        {
+            Name = name;
+            Price = price;
+            QuantityInStock = quantityInStock;
+        }
+    }
+
+    public class Inventory
+    {
+            private List<Product> listOfProducts = new List<Product>();
+    }
+
+
+
+}
